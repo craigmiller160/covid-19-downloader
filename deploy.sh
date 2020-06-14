3159b3e5102c3c80b7eac2b96bf49670e8f03c64
Craig Miller <craigmiller160@gmail.com>
2020-06-14 16:09:07 -0400

    Cleaned up deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-name=$(cat package.json | grep name | sed 's/"name":\s\?"//g' | sed 's/",$//g')
-version=$(cat package.json | grep version | sed 's/"version":\s\?"//g' | sed 's/",$//g')
+name=$(cat package.json | grep name | sed 's/^\s*"name":\s\?"//g' | sed 's/",$//g')
+version=$(cat package.json | grep version | sed 's/^\s*"version":\s\?"//g' | sed 's/",$//g')
 
-sudo docker build -t "localhost:32000/$name:v$version"
+echo "Building $name:$version"
+
+sudo docker build -t localhost:32000/$name:$version .
