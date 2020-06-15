7c3313c9eac537da038d062db79335fc9e814225
Craig Miller <craigmiller160@gmail.com>
2020-06-14 21:44:41 -0400

    Some more fixes to deployment process

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,14 @@
 
 name=$(cat package.json | grep name | sed 's/^\s*"name":\s\?"//g' | sed 's/",$//g')
 version=$(cat package.json | grep version | sed 's/^\s*"version":\s\?"//g' | sed 's/",$//g')
+registry=localhost:32000
+tag=$registry/$name:$version
 
 echo "Building $name:$version"
 
 cd deploy
 sudo docker build \
   --network=host \
-  -t localhost:32000/$name:$version \
+  -t $tag \
   .
+sudo docker push $tag
