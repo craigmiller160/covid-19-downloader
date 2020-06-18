d34e44b643e362dd7fa978cbd6d177d2e7e664a2
Craig Miller <craigmiller160@gmail.com>
2020-06-18 16:50:24 -0400

    Added retry behavior for ECDC data

--- src/download/downloadEcdcData.js
+++ src/download/downloadEcdcData.js
@@ -1,15 +1,44 @@
 const axios = require('axios');
 const moment = require('moment');
 const TraceError = require('trace-error');
+const retry = require('retry');
 const { logger } = require('@craigmiller160/covid-19-config-mongo');
 
 const url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/';
 
+const executeDownload = async (currentAttempt) => {
+    logger.debug(`Attempt #${currentAttempt} to download ECDC data`);
+    const res = await axios.get(url);
+    if (res.data.map) {
+        return res.data;
+    }
+    throw new Error('ECDC API did not return valid response, may be down.');
+};
+
+const attemptExecuteDownload = () => new Promise((resolve, reject) => {
+    const operation = retry.operation({
+        retries: process.env.DOWNLOAD_RETRY_ATTEMPTS,
+        minTimeout: process.env.DOWNLOAD_RETRY_WAIT
+    });
+
+    operation.attempt(async (currentAttempt) => {
+        try {
+            const data = await executeDownload(currentAttempt);
+            resolve(data);
+        } catch (ex) {
+            if (operation.retry(ex)) {
+                return;
+            }
+            reject(ex);
+        }
+    });
+});
+
 const downloadEcdcData = async () => {
     logger.info('Attempting to download ECDC data');
     try {
-        const res = await axios.get(url);
-        const newData = res.data.records.map((record) => ({
+        const data = await attemptExecuteDownload();
+        const newData = data.records.map((record) => ({
             date: moment(record.dateRep, 'DD/MM/YYYY').toDate(),
             newCases: parseInt(record.cases),
             newDeaths: parseInt(record.deaths),
