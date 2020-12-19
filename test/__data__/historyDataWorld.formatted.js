42beb5241403f9c49198beb88d0302c9c13bc977
Craig Miller <craigmiller160@gmail.com>
2020-12-19 15:45:44 -0500

    Setup more test data

---
+++ test/__data__/historyDataWorld.formatted.js
@@ -0,0 +1,76 @@
+/*
+ *     covid-19-downloader
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = [
+    {
+        date: new Date('2020-01-220T00:00:00Z'),
+        location: 'World',
+        newCases: 555,
+        newDeaths: 17,
+        totalCases: 555,
+        totalDeaths: 17
+    },
+    {
+        date: new Date('2020-01-230T00:00:00Z'),
+        location: 'World',
+        newCases: 99,
+        newDeaths: 1,
+        totalCases: 654,
+        totalDeaths: 18
+    },
+    {
+        date: new Date('2020-01-240T00:00:00Z'),
+        location: 'World',
+        newCases: 287,
+        newDeaths: 8,
+        totalCases: 941,
+        totalDeaths: 26
+    },
+    {
+        date: new Date('2020-01-250T00:00:00Z'),
+        location: 'World',
+        newCases: 0,
+        newDeaths: 0,
+        totalCases: 1434,
+        totalDeaths: 42
+    },
+    {
+        date: new Date('2020-01-260T00:00:00Z'),
+        location: 'World',
+        newCases: 684,
+        newDeaths: 14,
+        totalCases: 2118,
+        totalDeaths: 56
+    },
+    {
+        date: new Date('2020-01-270T00:00:00Z'),
+        location: 'World',
+        newCases: 809,
+        newDeaths: 26,
+        totalCases: 2927,
+        totalDeaths: 82
+    },
+    {
+        date: new Date('2020-01-280T00:00:00Z'),
+        location: 'World',
+        newCases: 2651,
+        newDeaths: 49,
+        totalCases: 5578,
+        totalDeaths: 131
+    }
+]
---
+++ test/__data__/historyDataWorld.raw.js
@@ -0,0 +1,38 @@
+/*
+ *     covid-19-downloader
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    cases: {
+        "1/22/20": 555,
+        "1/23/20": 654,
+        "1/24/20": 941,
+        "1/25/20": 1434,
+        "1/26/20": 2118,
+        "1/27/20": 2927,
+        "1/28/20": 5578
+    },
+    deaths: {
+        "1/22/20": 17,
+        "1/23/20": 18,
+        "1/24/20": 26,
+        "1/25/20": 42,
+        "1/26/20": 56,
+        "1/27/20": 82,
+        "1/28/20": 131
+    }
+};
