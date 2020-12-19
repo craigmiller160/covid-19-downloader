534d6956783e82ee307b48389dc831ddc79e47f9
Craig Miller <craigmiller160@gmail.com>
2020-12-19 15:23:11 -0500

    Setup more test data

---
+++ test/currentDataAllCountries.formatted.js
@@ -0,0 +1,56 @@
+/*
+ *     covid-19-downloader
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = [
+    {
+        location: 'Afghanistan',
+        displayLocation: 'Afghanistan',
+        population: 39331210,
+        totalCases: 50536,
+        totalCasesPerMillion: 1285,
+        totalDeaths: 2054,
+        totalDeathsPerMillion: 52
+    },
+    {
+        location: 'Albania',
+        displayLocation: 'Albania',
+        population: 2876324,
+        totalCases: 52542,
+        totalCasesPerMillion: 18267,
+        totalDeaths: 1074,
+        totalDeathsPerMillion: 373
+    },
+    {
+        location: 'Algeria',
+        displayLocation: 'Algeria',
+        population: 44216365,
+        totalCases: 94781,
+        totalCasesPerMillion: 2144,
+        totalDeaths: 2659,
+        totalDeathsPerMillion: 60
+    },
+    {
+        location: 'Andorra',
+        displayLocation: 'Andorra',
+        population: 77323,
+        totalCases: 7560,
+        totalCasesPerMillion: 97772,
+        totalDeaths: 80,
+        totalDeathsPerMillion: 1035
+    }
+];
