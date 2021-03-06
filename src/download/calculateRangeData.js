a044ef8c69539a14021c3dfe9e49b9c2c93256af
Craig <craigmiller160@gmail.com>
2021-03-06 17:42:16 -0500

    Took notes

--- src/download/calculateRangeData.js
+++ src/download/calculateRangeData.js
@@ -1,6 +1,9 @@
 
 /*
- * 1) Identify the month and year of the
+ * 1) Identify the month and year of the record.
+ * 2) Track the "current" month and year.
+ * 3) Record the first totals for month/year as "start" in the output.
+ * 4) When the month changes, retrieve the previous record value as the last value for that month/year
  */
 
 const calculateRangeData = (data) => {
