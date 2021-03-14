264ab37b92d97e9b046fd84ba6e9440740e1f1ea
Craig Miller <craigmiller160@gmail.com>
2021-03-14 14:48:33 -0400

    Tested add population data

--- test/download/stateDataCalculations.test.js
+++ test/download/stateDataCalculations.test.js
@@ -16,8 +16,75 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+const moment = require('moment');
+const {
+    addPopulationData
+} = require('../../src/download/stateDataCalculations');
+
+const censusData = [
+    {
+        displayLocation: 'Alaska',
+        population: 1000
+    },
+    {
+        displayLocation: 'Alabama',
+        population: 2000
+    }
+];
+
+const historyData = [
+    {
+        location: 'AK',
+        date: moment('2020-01-01').toDate(),
+        totalCases: 100
+    },
+    {
+        location: 'AK',
+        date: moment('2020-01-02').toDate(),
+        totalCases: 200
+    },
+    {
+        location: 'AL',
+        date: moment('2020-01-01').toDate(),
+        totalCases: 300
+    },
+    {
+        location: 'AL',
+        date: moment('2020-01-02').toDate(),
+        totalCases: 400
+    }
+];
+
+const expectedWithPop = [
+    {
+        location: 'AK',
+        date: moment('2020-01-01').toDate(),
+        totalCases: 100,
+        population: 1000
+    },
+    {
+        location: 'AK',
+        date: moment('2020-01-02').toDate(),
+        totalCases: 200,
+        population: 1000
+    },
+    {
+        location: 'AL',
+        date: moment('2020-01-01').toDate(),
+        totalCases: 300,
+        population: 2000
+    },
+    {
+        location: 'AL',
+        date: moment('2020-01-02').toDate(),
+        totalCases: 400,
+        population: 2000
+    }
+]
+
 describe('stateDataCalculations', () => {
     it('addPopulationData', () => {
-        throw new Error();
+        const result = addPopulationData(historyData, censusData);
+        expect(result).toEqual(expectedWithPop);
     });
 });
